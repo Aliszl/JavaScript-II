d2abab740011b6e8a0bc36a3099677b1db0558fe
Alison Law <alisparklaw@gmail.com>
2019-10-15 15:46:33 +0100

    callbacks complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,64 +1,96 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+function multiplyNums(x, y, cb) {
+  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x,y);
+}
+function multiply(a,b){
+  return a*b;
+}
+console.log(multiplyNums(2,10,multiply));
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+// Create a higher order function and invoke the callback function to test your work.
+//  You have been provided an example of a problem and a solution to see how this works 
+//  with our items array.  Study both the problem and the solution to figure out the 
+//  rest of the problems.
 
-/* 
+// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
   // GIVEN THIS PROBLEM:
+  // function firstItem(arr, cb) {
+  //   // firstItem passes the first item of the given array to the callback function.
+  // }
 
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
-
-  // SOLUTION:
-
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
-
+  
   // NOTES ON THE SOLUTION:
-
   // firstItem is a higher order function.
   // It expects a callback (referred to as `cb`) as its second argument.
   // To test our solution, we can use the given `items` array and a variety of callbacks.
   // Note how callbacks can be declared separately, or inlined.
 
   // TEST 1 (inlined callback):
+  // const test1 = firstItem(items, item => `I love my ${item}!`);
+  // console.log(test1); // "I love my Pencil!"
+  function sumNums(x, y, cb) {
+    // sumNums adds two numbers (x, y) and passes the result to the callback.
+    return cb(x,y);
+  }
+  function sum(a,b){
+    return a + b;
+  }
+  console.log(sumNums(5,6,sum));
 
-  const test1 = firstItem(items, item => `I love my ${item}!`);
-  console.log(test1); // "I love my Pencil!"
-
+  const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+  
+  // SOLUTION:
+  function firstItem(arr, cb) {
+    return cb(arr[0]);
+  }
   // TEST 2 (declaring callback before hand):
-
   function logExorbitantPrice(article) {
     return `this ${article} is worth a million dollars!`;
   };
 
   const test2 = firstItem(items, logExorbitantPrice);
   console.log(test2); // "this Pencil is worth a million dollars!"
-*/
 
+// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'Pencil'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+   return cb(arr);
 }
+
+function getLengthcb(blob){
+  return blob.length;
+}
+console.log("testing getLength function ", );
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr);
 }
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+function lastItem(placeholder){
+  return placeholder[placeholder.length-1];
 }
-
-function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+console.log(`Testing last function: `, last(items, lastItem))
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item, list);
+  
 }
+function checkArray(itemAnon,list){
+   return list.includes(itemAnon);
+  
+}
+console.log("testing contains function ", contains("Pencil", items, checkArray));
+
+
+  // const newArray = items.filter(contains)=>{
+  //   return contains.
+  // }
+
 
 /* STRETCH PROBLEM */
 
